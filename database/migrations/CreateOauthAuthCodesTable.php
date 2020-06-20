ad0f814942e6b003b9736590404ed8314e618857
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-06-20 13:48:14 +0600

    Renamed migration files name

--- database/migrations/CreateOauthAuthCodesTable.php
+++ database/migrations/CreateOauthAuthCodesTable.php
@@ -3,7 +3,6 @@
 use CodexShaper\Database\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateOauthAuthCodesTable extends Migration
 {
